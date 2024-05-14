47152eda5864894f1a0c664c27291f2015a36264
Guy Levitsky <guy.l@datarails.com>
2024-05-14 12:34:55 +0300

    State Machine

--- thermal_camera_static_view.py
+++ thermal_camera_static_view.py
@@ -1,3 +1,4 @@
+import math
 from dataclasses import dataclass
 
 import numpy as np
@@ -43,37 +44,164 @@ def get_thermal_eye_instance_thermal_cam(device_id):
         cap=cap
     )
 
+
+
+def detect_motion(frame, last_mean):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    result = np.abs(np.mean(gray) - last_mean)
+    last_mean = np.mean(gray)
+
+    print(result)
+    if result > 0.3:
+        print("Motion detected!")
+        print("Started recording.")
+        return True, last_mean
+    return False, last_mean
+
+
+def plant_state_name_in_frame(frame, state):
+    # setup text
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    text = state
+
+    # get boundary of this text
+    text_size = cv2.getTextSize(text, font, 1, 2)[0]
+
+    # get coords based on boundary
+    textX = (frame.shape[1] - text_size[0]) // 2
+    textY = (frame.shape[0] + text_size[1]) // 5
+
+    # add text centered on image
+    cv2.putText(frame, text, (textX, textY), font, 1, (255, 255, 255), 2)
+
+
+BEAM_RADIUS = 42
+COLOR_RED = (0, 0, 255)
+COLOR_BLACK = (0, 0, 0)
+
+ARROW_THICKNESS = 2
+
+CIRCLE_THICKNESS = 2
+
+
+def draw_light_beam(frame):
+    center_of_circle = (frame.shape[1] // 2, frame.shape[0] // 2)
+
+    cv2.circle(frame, center_of_circle, BEAM_RADIUS, COLOR_RED, CIRCLE_THICKNESS)
+
+
+def draw_moving_contours(frame, contours):
+    for contour, area in contours:
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+
+def find_closest_target(frame, contours):
+    center_of_circle = (frame.shape[1] // 2, frame.shape[0] // 2)
+
+    closest = None
+    closest_distance = None
+    
+    for c, area in contours:
+        x, y, w, h = cv2.boundingRect(c)
+        
+        dx2 = (center_of_circle[0] - x) ** 2
+        dy2 = (center_of_circle[1] - y) ** 2
+        
+        distance = math.sqrt(dx2 + dy2)
+        
+        if closest_distance is None or distance < closest_distance:
+            closest = c
+            closest_distance = distance
+    
+    return closest
+        
+
+def is_target_in_circle(frame, target):
+    pass
+
+
+def draw_vector_to_target(frame, target_c):
+    x, y, w, h = cv2.boundingRect(target_c)
+    cv2.rectangle(frame, (x, y), (x + w, y + h), COLOR_BLACK, 2)
+
+    target_center = (x + (w // 2), y + (h // 2))
+
+    center_of_circle = (frame.shape[1] // 2, frame.shape[0] // 2)
+
+    cv2.arrowedLine(frame, center_of_circle, target_center,
+                    COLOR_BLACK, ARROW_THICKNESS)
+
+
 if __name__ == "__main__":
     # thermal_eye = get_thermal_eye_instance_video_test()
     thermal_eye = get_thermal_eye_instance_thermal_cam(0)
 
-    backgroundObject = cv2.BackgroundSubtractorMOG2()
-    # backgroundObject.setHistory(2)
+    cap = thermal_eye.cap
 
-    kernel = np.ones((3, 3), np.uint8)
+    FRAME_WIDTH = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    FRAME_HEIGHT = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    FRAME_TOTAL_AREA = FRAME_WIDTH * FRAME_HEIGHT
+    IN_MOVEMENT_TH = FRAME_TOTAL_AREA // 2
+
+    fg_backgorund = cv2.createBackgroundSubtractorMOG2(history=2)
+
+    # choose codec according to format needed
+    # fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    # video_writer = cv2.VideoWriter('output.mp4', fourcc, 20.0, (w, h))
+    # count = 0
 
     while True:
+        state = None
         ret, frame = thermal_eye.cap.read()
 
-        if not ret:
-            print('no cap video.')
+        fg_mask = fg_backgorund.apply(frame)
+        th = cv2.threshold(fg_mask, 0, 100, cv2.THRESH_BINARY)[1]
+        contours, hierarchy = cv2.findContours(th, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+        contour_size_tup = [(c, cv2.contourArea(c)) for c in contours]
+
+        MIN_AREA_TO_CONSIDER = 20
+
+        area_in_movement = sum([c[1] for c in contour_size_tup])
+        is_camera_in_movement = area_in_movement > IN_MOVEMENT_TH
+
+        # filter small movements
+        filtered_contours = [c for c in contour_size_tup if c[1] > MIN_AREA_TO_CONSIDER]
+
+        if is_camera_in_movement:
+            # camera in movement state
+            state = 'IN_MOVEMENT'
+        else:
+            state = 'SEARCHING THE RING'
+            draw_light_beam(frame)
+
+            contours_sorted = sorted(filtered_contours, key=lambda c: -c[1])[:3]
+
+            draw_moving_contours(frame, contours_sorted)
+
+            target = find_closest_target(frame, contours_sorted)
+            
+            draw_vector_to_target(frame, target)
+            
+            if is_target_in_circle(frame, target):
+                state = "FOUND HOBBIT - LIGHT THE BEAM"
+
+
+        if state:
+            plant_state_name_in_frame(frame, state)
+
+        cv2.imshow('frame', frame)
+
+        # video_writer.write(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        # cv2.imwrite("frame_%d.tiff" % count, frame)  # save frame as JPEG file
+        # count += 1
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-        fg_mask = backgroundObject.apply(frame)
+    thermal_eye.cap.release()
+    cv2.destroyAllWindows()
 
-        # Find contours
-        contours, hierarchy = cv2.findContours(fg_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        # print(contours)
-        frame_ct = cv2.drawContours(frame, contours, -1, (0, 255, 0), 2)
-        # Display the resulting frame
-        cv2.imshow('Frame_final', frame_ct)
-
-        # cv2.threshold(fgmask, 20, 255, cv2.THRESH_BINARY)
-        # fgmask = cv2.erode(fgmask, kernel, iterations=1)
-
-        # cv2.imshow("frame", frame)
-        # cv2.imshow("fgmask", fgmask)
-
-        if cv2.waitKey(33) == ord('q'):
-            break
-
+    # video_writer.release()
