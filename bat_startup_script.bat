c1db7cb13a9510739b2fccc6f20539615c3917fd
Guy Levitsky <guy.levitsky@gmail.com>
2024-06-01 21:09:21 +0300

    startup script

--- bat_startup_script.bat
+++ bat_startup_script.bat
@@ -1,3 +1,3 @@
 @echo off
-"C:\Users\noam\sauronism\opencv-thermal-evil\venv311\Scripts\python.exe" "C:\Users\noam\sauronism\opencv-thermal-evil\startup_script.py"
+"C:\Users\noam\sauronism\opencv-thermal-evil\venv311\Scripts\python.exe" "C:\Users\noam\sauronism\opencv-thermal-evil\main.py"
 pause
