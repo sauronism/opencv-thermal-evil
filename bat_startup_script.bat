2820c71b2c4fa7af57fb3a78c5bb77e2b38a2ab2
Guy Levitsky <guy.levitsky@gmail.com>
2024-06-01 18:35:01 +0300

    startup script

---
+++ bat_startup_script.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Users\noam\sauronism\opencv-thermal-evil\venv311\Scripts\python.exe" "C:\Users\noam\sauronism\opencv-thermal-evil\startup_script.py"
+pause
---
+++ startup_script.py
@@ -0,0 +1,3 @@
+
+if __name__ == '__main__':
+    print('42 is the answer upon startup.')
